77f9ce769a121985dfbfb1ab1903621ea7ec0cdd
KhanhDNg <khanhdnguyen@hotmail.com>
2021-03-10 17:58:12 +1100

    fixed null values

--- bookscrape/bookscrape/items.py
+++ bookscrape/bookscrape/items.py
@@ -11,30 +11,26 @@ import re
 def num_pages_extractor(num_pages):
     if num_pages:
         return num_pages.split()[0]
-    return None
 
 def publish_data_extractor(publish_date):
     if publish_date:
         date_content = [s for s in publish_date if 'published' in s.lower()][0]
         return date_content.split('\n        ')[2]
-    return None
 
 def first_publish_date_extractor(first_publish_date):
     if first_publish_date:
         date_content = [s for s in first_publish_date if 'first published' in s.lower()][0]
         return re.split('\(|\)|published ', date_content)[2]
-    return None
+
 def isbn13_filter(isbn13):
     if isbn13 and len(str(isbn13)) == 13 and isbn13.isdigit():
         return isbn13
-    return None
 
 def isbn_filter(isbn):
     if isbn and len(str(isbn)) == 10 and isbn.isdigit():
         return isbn
-    return None
 
-def extract_ratings(txt):
+def extract_rated_5(txt):
     """Extract the rating histogram from embedded Javascript code
 
         The embedded code looks like this:
@@ -52,17 +48,57 @@ def extract_ratings(txt):
         return None
     rating_code = rating_code[0]
     rating_array = rating_code[rating_code.index("[") + 1 : rating_code.index("]")]
-    ratings = {5 - i:int(x) for i, x in enumerate(rating_array.split(","))}
-    return ratings
+    rated_5 = rating_array.split(', ')[0]
+    return rated_5
+
+def extract_rated_4(txt):
+    codelines = "".join(txt).split(";")
+    rating_code = [line.strip() for line in codelines if "renderRatingGraph" in line]
+    if not rating_code:
+        return None
+    rating_code = rating_code[0]
+    rating_array = rating_code[rating_code.index("[") + 1 : rating_code.index("]")]
+    rated_4 = rating_array.split(', ')[1]
+    return rated_4
+
+def extract_rated_3(txt):
+    codelines = "".join(txt).split(";")
+    rating_code = [line.strip() for line in codelines if "renderRatingGraph" in line]
+    if not rating_code:
+        return None
+    rating_code = rating_code[0]
+    rating_array = rating_code[rating_code.index("[") + 1 : rating_code.index("]")]
+    rated_3 = rating_array.split(', ')[2]
+    return rated_3
+
+def extract_rated_2(txt):
+    codelines = "".join(txt).split(";")
+    rating_code = [line.strip() for line in codelines if "renderRatingGraph" in line]
+    if not rating_code:
+        return None
+    rating_code = rating_code[0]
+    rating_array = rating_code[rating_code.index("[") + 1 : rating_code.index("]")]
+    rated_2 = rating_array.split(', ')[3]
+    return rated_2
+
+def extract_rated_1(txt):
+    codelines = "".join(txt).split(";")
+    rating_code = [line.strip() for line in codelines if "renderRatingGraph" in line]
+    if not rating_code:
+        return None
+    rating_code = rating_code[0]
+    rating_array = rating_code[rating_code.index("[") + 1 : rating_code.index("]")]
+    rated_1 = rating_array.split(', ')[4]
+    return rated_1
 
 class BookscrapeItem(scrapy.Item):
     bookId = scrapy.Field()
-    title       = scrapy.Field(input_processor=MapCompose(str.strip))
-    author      = scrapy.Field(input_processor=MapCompose(str.strip))
-    num_ratings = scrapy.Field(input_processor=MapCompose(str.strip, int))
-    num_reviews = scrapy.Field(input_processor=MapCompose(str.strip, int))
-    num_pages   = scrapy.Field(input_processor=MapCompose(str.strip, num_pages_extractor, int))
-    avg_rating = scrapy.Field(input_processor=MapCompose(str.strip, float))
+    title = scrapy.Field(input_processor=MapCompose(str.strip))
+    author = scrapy.Field(input_processor=MapCompose(str.strip))
+    num_ratings = scrapy.Field(input_processor=MapCompose(str.strip))
+    num_reviews = scrapy.Field(input_processor=MapCompose(str.strip))
+    num_pages = scrapy.Field(input_processor=MapCompose(str.strip, num_pages_extractor))
+    avg_rating = scrapy.Field(input_processor=MapCompose(str.strip))
 
     language = scrapy.Field(input_processor=MapCompose(str.strip))
     publish_date = scrapy.Field(input_processor=publish_data_extractor)
@@ -74,11 +110,16 @@ class BookscrapeItem(scrapy.Item):
     places = scrapy.Field(output_processor=Identity())
     awards = scrapy.Field(output_processor=Identity())
 
-    genres = scrapy.Field(output_processor=Compose(set, list))
+    genres = scrapy.Field(input_processor=MapCompose(str.strip))
     isbn13 = scrapy.Field(input_processor=MapCompose(str.strip, isbn13_filter))
     isbn = scrapy.Field(input_processor=MapCompose(str.strip, isbn_filter))
 
     # All ratings (1-5 stars)
-    ratings = scrapy.Field(input_processor=MapCompose(extract_ratings))
+    rated_5 = scrapy.Field(input_processor=MapCompose(extract_rated_5))
+    rated_4 = scrapy.Field(input_processor=MapCompose(extract_rated_4))
+    rated_3 = scrapy.Field(input_processor=MapCompose(extract_rated_3))
+    rated_2 = scrapy.Field(input_processor=MapCompose(extract_rated_2))
+    rated_1 = scrapy.Field(input_processor=MapCompose(extract_rated_1))
+
 
     pass
--- bookscrape/bookscrape/pipelines.py
+++ bookscrape/bookscrape/pipelines.py
@@ -26,23 +26,86 @@ class BookscrapePipeline:
     def create_table(self):
         self.curr.execute("""DROP TABLE IF EXISTS books_tb""")
         self.curr.execute("""create table books_tb(
+                            id text,
                             title text,
                             author text,
                             num_ratings text,
                             num_reviews text,
-                            num_pages text 
+                            num_pages text,
+                            avg_rating text,
+                            language text,
+                            publish_date text,
+                            first_publish_date text,
+                            series text,
+                            characters text,
+                            places text,
+                            awards text,
+                            genres text,
+                            isbn13 text,
+                            isbn text,
+                            rated_5 text,
+                            rated_4 text,
+                            rated_3 text,
+                            rated_2 text,
+                            rated_1 text
                             )""")
     def store_db(self, item):
-        self.curr.execute("""insert into books_tb values (%s,%s,%s,%s,%s)""", (
-            item['bookTitle'],
-            item['author'],
-            item['num_ratings'],
-            item['num_reviews'],
-            item['num_pages']
-        ))
+        self.curr.execute("""INSERT INTO books_tb VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+                            (item['bookId'][0],
+                            item['title'][0],
+                            item['author'],
+                            item['num_ratings'][0],
+                            item['num_reviews'][0],
+                            item['num_pages'][0],
+                            item['avg_rating'][0],
+                            item['language'][0],
+                            item['publish_date'][0],
+                            item['first_publish_date'][0],
+                            item['series'][0],
+                            item['characters'][0],
+                            item['places'][0],
+                            item['awards'][0],
+                            item['genres'][0],
+                            item['isbn13'][0],
+                            item['isbn'][0],
+                            item['rated_5'][0],
+                            item['rated_4'][0],
+                            item['rated_3'][0],
+                            item['rated_2'][0],
+                            item['rated_1'][0]
+                            ))
         self.conn.commit()
 
     def process_item(self, item, spider):
-        # self.store_db(item)
-        print(item)
-        return item
+        item.setdefault('title', 'Null')
+        item.setdefault('author', 'Null')
+        item.setdefault('num_ratings', 'Null')
+        item.setdefault('num_reviews', 'Null')
+        item.setdefault('avg_rating', 'Null')
+        item.setdefault('num_pages', 'Null')
+        item.setdefault('language', 'Null')
+        item.setdefault('publish_date', 'Null')
+        item.setdefault('first_publish_date', 'Null')
+        item.setdefault('series', 'Null')
+        item.setdefault('characters', 'Null')
+        item.setdefault('places', 'Null')
+        item.setdefault('awards', 'Null')
+        item.setdefault('genres', 'Null')
+        item.setdefault('isbn13', 'Null')
+        item.setdefault('isbn', 'Null')
+        item.setdefault('rated_5', 'Null')
+        item.setdefault('rated_4', 'Null')
+        item.setdefault('rated_3', 'Null')
+        item.setdefault('rated_2', 'Null')
+        item.setdefault('rated_1', 'Null')
+
+        self.store_db(item)
+        # print(item['bookId'],item['title'], item['author'], item['num_ratings'],
+        #     item['num_reviews'], item['num_pages'], item['avg_rating'], item['language'], item['publish_date'],
+        #     item['first_publish_date'], item['series'], item['characters'], item['places'],
+        #     item['awards'], item['genres'], item['isbn13'], item['isbn'], item['ratings'])
+
+        # print("5*: "+str(item['rated_5'][0])+"\n4*: "+ str(item['rated_4'][0]) +"\n3*: "+ str(item['rated_3'][0]) +"\n2*: "+ str(item['rated_2'][0]) +"\n1*: "+ str(item['rated_1'][0]))
+
+        # return item
+
